5ae35bedead818d014a06386113c14043fc56e71
DeetDeet <deet1022@gmail.com>
2016-02-18 18:36:21 +0900

    add file

---
+++ Csharp-test-code/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sub;
+
+namespace Csharp_test_code {
+	class Program {
+		static void Main(string[] args) {
+			Circle circle = new Circle();
+			circle.Print(4);
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Csharp-test-code/Sub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Sub {
+	class Circle {
+		double pi = 3.14;
+
+		double getArea(double radius) {
+			return radius * radius * pi;
+		}
+
+		public void Print(double value) {
+			Console.WriteLine(getArea(value));
+		}
+	}
+}
